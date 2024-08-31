224569b52b248db306c7d47c0ca6209319fa1b77
Santiago Anwandter Meza <64712624+santii0135@users.noreply.github.com>
2024-08-31 12:23:29 -0400

    Create selectionSort.cpp

---
+++ selectionSort.cpp
@@ -0,0 +1,37 @@
+// C++ program for implementation of
+// selection sort
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function for Selection sort
+void selectionSort(int arr[], int n)
+{
+    // One by one move boundary of
+    // unsorted subarray
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Find the minimum element in
+        // unsorted array
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+        // Swap the found minimum element
+        // with the first element
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// This is code is contributed by rathbhupendra
