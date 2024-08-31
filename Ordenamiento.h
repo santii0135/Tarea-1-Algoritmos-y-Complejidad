8c9a815ff1183039b53d5c2feacca1fcb372c529
Santiago Anwandter Meza <64712624+santii0135@users.noreply.github.com>
2024-08-31 13:02:43 -0400

    Create Ordenamiento.h

---
+++ Ordenamiento.h
@@ -0,0 +1,12 @@
+#ifndef ORDENAMIENTO_H
+#define ORDENAMIENTO_H
+
+#include <vector>
+
+// Declaraciones de funciones para algoritmos de ordenamiento
+void selectionSort(std::vector<int>& data);
+void quickSort(std::vector<int>& data, int low, int high);
+void mergeSort(std::vector<int>& data, int left, int right);
+void multiply(int A[][N], int B[][N], int C[][N]);
+
+#endif 
